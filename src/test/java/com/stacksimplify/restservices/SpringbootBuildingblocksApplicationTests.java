e6e245616952d57ee621e69e8d63846c87146d62
quanvdm <vdmquan@gmail.com>
2021-08-18 22:24:02 +0700

    JPA Restful service development completed!

--- src/test/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplicationTests.java
+++ src/test/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplicationTests.java
@@ -1,6 +1,6 @@
 package com.stacksimplify.restservices;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
