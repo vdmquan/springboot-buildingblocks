27b024920762d680a01070a609a6c98af503b902
quanvdm <vdmquan@gmail.com>
2021-08-23 20:37:13 +0700

    GlobalExceptionHander first commit

--- src/main/java/com/stacksimplify/restservices/controllers/UserController.java
+++ src/main/java/com/stacksimplify/restservices/controllers/UserController.java
@@ -3,10 +3,14 @@ package com.stacksimplify.restservices.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,11 +23,13 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.stacksimplify.restservices.entities.User;
 import com.stacksimplify.restservices.exceptions.UserExistsException;
+import com.stacksimplify.restservices.exceptions.UserNameNotFoundException;
 import com.stacksimplify.restservices.exceptions.UserNotFoundException;
 import com.stacksimplify.restservices.services.UserService;
 
 //Controller --
 @RestController
+@Validated
 public class UserController {
 
 	@Autowired
@@ -36,7 +42,7 @@ public class UserController {
 
 	//Create User Method, @RequestBody Annotation, @PostMapping Annotation
 	@PostMapping("/users")
-	public ResponseEntity<Void> createUser(@RequestBody User user, UriComponentsBuilder builder) {
+	public ResponseEntity<Void> createUser(@Valid @RequestBody User user, UriComponentsBuilder builder) {
 		try {
 			userService.createUser(user);
 			HttpHeaders headers = new HttpHeaders();
@@ -49,7 +55,7 @@ public class UserController {
 	}
 
 	@GetMapping("/users/{id}")
-	public Optional<User> getUserById(@PathVariable("id") Long id){
+	public Optional<User> getUserById(@PathVariable("id") @Min(1) Long id){
 		try {
 			return userService.getUserById(id);
 		} catch (UserNotFoundException ex) {
@@ -71,8 +77,12 @@ public class UserController {
 		userService.deleteUserById(id);
 	}
 
+	// getUserByUsername
 	@GetMapping("/users/byusername/{username}")
-	public User getUserByUsername(@PathVariable("username") String username) {
-		return userService.getUserByUsername(username);
+	public User getUserByUsername(@PathVariable("username") String username) throws UserNameNotFoundException {
+		User user =  userService.getUserByUsername(username);
+		if(user == null)
+			throw new UserNameNotFoundException("Username: '" + username + "' not found in User repository");
+		return user;
 	}
 }
